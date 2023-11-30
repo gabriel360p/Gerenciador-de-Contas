f8dbad3c377b6cc973d4231883a9c3ed8ac34fc0
lorium17 <109006007+gabriel360p@users.noreply.github.com>
2023-11-30 17:34:43 -0300

    começo da dash

--- front-gerenciador/src/router/index.js
+++ front-gerenciador/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AuthView from '../views/AuthView.vue'
+import PainelView from '../views/DashView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: AuthView
+  },
+  {
+    path: '/painel',
+    name: 'painel',
+    component: PainelView
   },
 ]
 
---
+++ front-gerenciador/src/views/DashView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>   
+        <NavbarComp/>
+
+
+    </div>
+</template>
+
+
+
+<script>
+import NavbarComp from '@/components/NavbarComp.vue';
+export default {
+    name: 'PainelView',
+    components: {
+        NavbarComp,
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
